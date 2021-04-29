5b7da67227336c7caecd90fdf7b4278e6b619f22
Canberk Keles <canberkkeles@sabanciuniv.edu>
2021-04-29 12:08:35 +0300

    Questions can be added to surveys

--- src/contracts/SurveyReward.sol
+++ src/contracts/SurveyReward.sol
@@ -8,7 +8,7 @@ contract SurveyReward {
     struct Survey {
         string title;
         address payable conductor;
-        int256 questionCount;
+        uint256 questionCount;
         bool open;
         mapping(uint256 => Question) questions;
     }
@@ -16,7 +16,7 @@ contract SurveyReward {
     event SurveyCreation(
         string title,
         address payable conductor,
-        int256 questionCount,
+        uint256 questionCount,
         bool open
     );
 
@@ -26,36 +26,48 @@ contract SurveyReward {
         mapping(uint256 => Answer) answers;
     }
 
-    event QuestionCreation(string text, uint256 answerCount);
+    event QuestionAdditionToSurvey(
+        string text,
+        uint256 answerCount,
+        uint256 surveyid,
+        uint256 questionCount
+    );
 
     struct Answer {
         address payable participant;
         string text;
     }
 
-    event AnswerCreate(address payable participant, string text);
-
     constructor() public {
         name = "Survey Rewarding System";
     }
 
-    function createSurvey(string memory _title, int256 _questioncount) public {
-        require(_questioncount > 0, "Survey has to have at least 1 question!");
+    function createSurvey(string memory _title) public {
+        require(bytes(_title).length != 0, "Survey title can not be empty");
 
-        surveys[surveyCount] = Survey(_title, msg.sender, _questioncount, true);
+        surveys[surveyCount] = Survey(_title, msg.sender, 0, true);
         surveyCount++;
-        emit SurveyCreation(_title, msg.sender, _questioncount, true);
+        emit SurveyCreation(_title, msg.sender, 0, true);
     }
 
-    function createQuestion(string memory _text) public {
+    function createQuestionToSurvey(string memory _text, uint256 _surveyid)
+        public
+    {
         require(bytes(_text).length != 0, "Question text can not be empty");
-        Question memory newQuestion = Question(_text, 0);
-        emit QuestionCreation(_text, 0);
-    }
-
-    function createAnswer(string memory _text) public {
-        require(bytes(_text).length != 0, "Answer text can not be empty");
-        Answer memory newAnswer = Answer(msg.sender, _text);
-        emit AnswerCreate(msg.sender, _text);
+        require(_surveyid < surveyCount, "Survey should exist");
+        Survey storage _survey = surveys[_surveyid];
+        require(
+            _survey.conductor == msg.sender,
+            "Only survey owner can add questions"
+        );
+        _survey.questions[_survey.questionCount] = Question(_text, 0);
+        _survey.questionCount++;
+        surveys[_surveyid] = _survey;
+        emit QuestionAdditionToSurvey(
+            _text,
+            _survey.questions[_survey.questionCount - 1].answerCount,
+            _surveyid,
+            _survey.questionCount
+        );
     }
 }
--- test/SurveyReward.test.js
+++ test/SurveyReward.test.js
@@ -35,7 +35,7 @@ contract("SurveyReward", ([conductor, participant]) => {
     let result;
     let surveyCount;
     before(async () => {
-      result = await surveyReward.createSurvey("Research on blockchain", 5, {
+      result = await surveyReward.createSurvey("Research on blockchain", {
         from: conductor,
       });
       surveyCount = await surveyReward.surveyCount();
@@ -51,41 +51,62 @@ contract("SurveyReward", ([conductor, participant]) => {
       );
       assert.equal(
         event.questionCount.toNumber(),
-        5,
+        0,
         "Question count is correct"
       );
       assert.equal(event.conductor, conductor, "Survey conductor is correct");
       assert.equal(event.open, true, "Survey status is correct");
 
       // NEGATIVE CASE
-      await surveyReward.createSurvey("Invalid survey", -1, {
+      await surveyReward.createSurvey("", {
         from: conductor,
       }).should.be.rejected;
     });
 
-    it("creates questions", async () => {
+    it("gets surveys", async () => {
       // POSITIVE CASE
-      result = await surveyReward.createQuestion("Blockchain question");
+      const survey = await surveyReward.surveys(0);
+      assert.equal(survey.title, "Research on blockchain", "Title is correct");
+      assert.equal(
+        survey.questionCount.toNumber(),
+        0,
+        "Question count is correct"
+      );
+      assert.equal(survey.conductor, conductor, "Conductor is correct");
+      assert.equal(survey.open, true, "Status is correct");
+    });
+
+    it("appends questions to survey", async () => {
+      // POSITIVE CASES
+      result = await surveyReward.createQuestionToSurvey(
+        "Question on blockchain",
+        0
+      );
       const event = result.logs[0].args;
       assert.equal(
         event.text,
-        "Blockchain question",
+        "Question on blockchain",
         "Question text is correct"
       );
-      assert.equal(event.answerCount.toNumber(), 0, "Question has no answers");
-      // NEGATIVE CASE
-      await surveyReward.createQuestion("").should.be.rejected;
-    });
+      assert.equal(
+        event.answerCount.toNumber(),
+        0,
+        "Answer count of question is correct"
+      );
+      assert.equal(event.surveyid.toNumber(), 0, "Survey id is correct");
+      assert.equal(
+        event.questionCount.toNumber(),
+        1,
+        "Question count is correct"
+      );
 
-    it("creates answers", async () => {
-      result = await surveyReward.createAnswer("Blockchain answer", {
+      // NEGATIVE CASES
+      await surveyReward.createQuestionToSurvey("", 0).should.be.rejected;
+      await surveyReward.createQuestionToSurvey("Valid question", -1).should.be
+        .rejected;
+      await surveyReward.createQuestionToSurvey("Valid question", 0, {
         from: participant,
-      });
-      const event = result.logs[0].args;
-      assert.equal(event.text, "Blockchain answer", "Answer text is correct");
-      assert.equal(event.participant, participant, "Participant is correct");
-      // NEGATIVE CASE
-      await surveyReward.createAnswer("").should.be.rejected;
+      }).should.be.rejected;
     });
   });
 });
