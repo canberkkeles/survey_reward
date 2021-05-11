6d8ae6139bbfb2e3c242c2377a8e4c2ee3cb4f18
izerburak <izerburak@gmail.com>
2021-05-11 19:57:39 +0300

    Modified index page.

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,6 +33,26 @@ const DUMMY_SURVEYS = [
     questionCount: "69",
     currentQuestion: "31",
   },
+  {
+    id: "survey4",
+    rewardPool: "120",
+    prize: "35",
+    title: "Fatih Survey",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    questionCount: "70",
+    currentQuestion: "44",
+  },
+  {
+    id: "survey5",
+    rewardPool: "6100",
+    prize: "200",
+    title: "CanberkBerry Survey",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    questionCount: "61",
+    currentQuestion: "61",
+  },
 ];
 const Main = () => {
   const [surveys, setSurveys] = useState(DUMMY_SURVEYS);
@@ -40,7 +60,7 @@ const Main = () => {
     <div>
       <Navbar />
       <p>Burak da burda</p>
-      <SurveyCardItems surveys={surveys} />
+      <SurveyCardItems style={{ height: "100%" }} surveys={surveys} />
     </div>
   );
 };
--- src/components/SurveyCardItems.js
+++ src/components/SurveyCardItems.js
@@ -1,10 +1,25 @@
 import React from "react";
 import SurveyCard from "./UI/SurveyCard";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  surveyCardContainer: {
+    flexGrow: 1,
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 100fr)",
+    columnGap: "15%",
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginTop: "2%",
+    gridRowGap: "5%",
+  },
+}));
 
 const SurveyCardItems = (props) => {
+  const classes = useStyles();
   const surveys = props.surveys;
   return (
-    <div>
+    <div className={classes.surveyCardContainer}>
       {surveys.map((survey) => (
         <SurveyCard
           key={survey.id}
